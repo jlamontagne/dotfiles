4b08097a91d0bc5cd5258e9059447695d3c42058
Josh Lamontagne <josh.lamonta@gmail.com>
2012-05-06 18:08:29 -0400

    xmonad: Clean things up a bit

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -26,7 +26,6 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 main = xmonad =<< myConfig
-
 myConfig = do
     checkTopicConfig myTopics myTopicConfig
     return =<< statusBar "xmobar" myPrettyPrinter toggleStrutsKey $ defaultConfig
@@ -54,28 +53,21 @@ myConfig = do
                 | (a, m) <- [(switchNthLastFocused myTopicConfig,""),(shiftNthLastFocused, "S-")]
                 , (i, k) <- zip [1..] "123456789"
             ]
-            -- TODO: Merge these.. maybe
-            -- ,("M-s"        , warpToCentre >> promptedGoto )
-            -- ,("M-S-s"      , warpToCentre >> promptedShift)
-            -- workaround
-            -- ,("M-<Tab>"    , switchNthLastFocused myTopicConfig . succ . length . W.visible . windowset =<< get )
-            -- ,("M-a"        , warpToCentre >> goToSelected gsConfig)
-            --
 
-        -- The list of all topics/workspaces of your xmonad configuration.  The order is
-        -- important, new topics must be inserted at the end of the list if you want
-        -- hot-restarting to work.
-        myTopics :: [Topic]
-        myTopics =
-            [ "dotfiles"
-            , "admin"
-            , "conf"
-            , "web"
-            , "tv"
-            , "music"
-            ]
+        myLayout = (
+            Tall 1 (3/100) (1/2) |||
+            Mirror (Tall 1 (3/100) (1/2)) |||
+            tabbed shrinkText defaultTheme |||
+            Full |||
+            spiral (6/7)) |||
+            noBorders (fullscreenFull Full)
 
-        myTopicConfig :: TopicConfig
+        myXPConfig = greenXPConfig { font = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15" }
+
+        -- The order is important, new topics must be inserted at the end of the
+        -- list if you want hot-restarting to work.
+        myTopics = [ "dotfiles", "admin", "conf", "web", "tv", "music" ]
+
         myTopicConfig = defaultTopicConfig {
             topicDirs = M.fromList $
                 [ ("dotfiles", ".dotfiles")
@@ -84,49 +76,31 @@ myConfig = do
             , topicActions = M.fromList $
                 [ ("dotfiles",   spawnShell)
                 -- , ("darcs",      spawnShell >*> 3)
-                -- , ("yi",         spawnShell >*> 3)
                 -- , ("haskell",    spawnShell >*> 2 >>
                 --                  spawnShellIn "wd/dev-haskell/ghc")
                 -- , ("xmonad",     spawnShellIn "wd/x11-wm/xmonad" >>
-                --                  spawnShellIn "wd/x11-wm/xmonad/contrib" >>
-                --                  spawnShellIn "wd/x11-wm/xmonad/utils" >>
-                --                  spawnShellIn ".xmonad" >>
                 --                  spawnShellIn ".xmonad")
                 -- , ("mail",       mailAction)
-                -- , ("irc",        ssh somewhere)
-                -- , ("admin",      ssh somewhere >>
-                --                  ssh nowhere)
                 -- , ("dashboard",  spawnShell)
-                -- , ("twitter",    spawnShell)
                 -- , ("web",        spawn browserCmd)
-                -- , ("movie",      spawnShell)
                 -- , ("documents",  spawnShell >*> 2 >>
                 --                  spawnShellIn "Documents" >*> 2)
-                -- , ("pdf",        spawn pdfViewerCmd)
                 ]
             , defaultTopicAction = const $ spawnShell >*> 3
             , defaultTopic = "dotfiles"
         }
 
+        goto = switchTopic myTopicConfig
+        promptedGoto = workspacePrompt myXPConfig goto
+        promptedShift = workspacePrompt myXPConfig $ windows . W.shift
         spawnShell = currentTopicDir myTopicConfig >>= spawnShellIn
-
         spawnShellIn dir = do
             t <- asks (terminal . config)
             spawnHere $ "cd " ++ dir ++ " && " ++ t
 
-        goto = switchTopic myTopicConfig
-
-        promptedGoto = workspacePrompt myXPConfig goto
-
-        promptedShift = workspacePrompt myXPConfig $ windows . W.shift
-
-        -- xmobar
-        --
-        -- Helper function which provides ToggleStruts keybinding. This is copied from
-        -- XMonad.Hooks.DynamicLog and probably needs to be exported with statusBar.
-        --
+        -- Copied from XMonad.Hooks.DynamicLog
         toggleStrutsKey :: XConfig t -> (KeyMask, KeySym)
-        toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b )
+        toggleStrutsKey XConfig{modMask = modm} = (modm, xK_b)
 
         myPrettyPrinter = xmobarPP
                             { ppCurrent = xmobarColor "yellow" "" . wrap "[" "]"
@@ -134,11 +108,7 @@ myConfig = do
                             , ppVisible = wrap "(" ")"
                             }
 
-        ------------------------------------------------------------------------
-        -- To find the property name associated with a program, use
-        -- > xprop | grep WM_CLASS
-        -- and click on the client you're interested in.
-        --
+        -- $ xprop | grep WM_CLASS
         myManageHook = composeAll
             [ className =? "Chromium"       --> doShift "2:web"
             , resource  =? "desktop_window" --> doIgnore
@@ -150,14 +120,3 @@ myConfig = do
             , className =? "VirtualBox"     --> doShift "4:vm"
             , isFullscreen --> (doF W.focusDown <+> doFullFloat)
             ]
-
-        myLayout = (
-            Tall 1 (3/100) (1/2) |||
-            Mirror (Tall 1 (3/100) (1/2)) |||
-            tabbed shrinkText defaultTheme |||
-            Full |||
-            spiral (6/7)) |||
-            noBorders (fullscreenFull Full)
-
-        myXPConfig :: XPConfig
-        myXPConfig = greenXPConfig { font = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15" }
