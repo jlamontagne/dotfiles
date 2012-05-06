2f2d16fd7f97ff184f4e048120c3b78aecf0b658
Josh Lamontagne <josh.lamonta@gmail.com>
2012-05-06 17:55:00 -0400

    xmonad: Define keybinds using EZConfig

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -21,7 +21,7 @@ import XMonad.Layout.Tabbed
 import XMonad.Prompt
 import XMonad.Prompt.Workspace
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
@@ -29,7 +29,7 @@ main = xmonad =<< myConfig
 
 myConfig = do
     checkTopicConfig myTopics myTopicConfig
-    return =<< statusBar "xmobar" myPrettyPrinter toggleStrutsKey defaultConfig
+    return =<< statusBar "xmobar" myPrettyPrinter toggleStrutsKey $ defaultConfig
         { workspaces = myTopics
         , manageHook = myManageHook
         , layoutHook = smartBorders $ myLayout
@@ -38,11 +38,30 @@ myConfig = do
         , terminal = "urxvt"
         , normalBorderColor = "white"
         , focusedBorderColor = "black"
-        , keys = myKeys
         , focusFollowsMouse = True
         , modMask = mod4Mask
-        }
+        } `additionalKeysP` myKeys
     where
+        myKeys =
+            [ ("M-S-<Return>", spawnShell)
+            , ("M-p", shellPromptHere myXPConfig)
+            , ("M-S-a", currentTopicAction myTopicConfig)
+            , ("M-s", promptedGoto)
+            , ("M-S-s", promptedShift)
+            ]
+            ++
+            [ ("M-"++m++[k], a i)
+                | (a, m) <- [(switchNthLastFocused myTopicConfig,""),(shiftNthLastFocused, "S-")]
+                , (i, k) <- zip [1..] "123456789"
+            ]
+            -- TODO: Merge these.. maybe
+            -- ,("M-s"        , warpToCentre >> promptedGoto )
+            -- ,("M-S-s"      , warpToCentre >> promptedShift)
+            -- workaround
+            -- ,("M-<Tab>"    , switchNthLastFocused myTopicConfig . succ . length . W.visible . windowset =<< get )
+            -- ,("M-a"        , warpToCentre >> goToSelected gsConfig)
+            --
+
         -- The list of all topics/workspaces of your xmonad configuration.  The order is
         -- important, new topics must be inserted at the end of the list if you want
         -- hot-restarting to work.
@@ -129,7 +148,8 @@ myConfig = do
             , className =? "MPlayer"        --> doFloat
             , resource  =? "skype"          --> doFloat
             , className =? "VirtualBox"     --> doShift "4:vm"
-            , isFullscreen --> (doF W.focusDown <+> doFullFloat)]
+            , isFullscreen --> (doF W.focusDown <+> doFullFloat)
+            ]
 
         myLayout = (
             Tall 1 (3/100) (1/2) |||
@@ -141,71 +161,3 @@ myConfig = do
 
         myXPConfig :: XPConfig
         myXPConfig = greenXPConfig { font = "-xos4-terminus-medium-r-normal--14-140-72-72-c-80-iso8859-15" }
-
-        myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
-            -- Start a terminal.
-            [ ((modMask .|. shiftMask, xK_Return), spawnShell)
-            -- TODO: Merge these.. maybe
-            -- ,("M-s"        , warpToCentre >> promptedGoto )
-            -- ,("M-S-s"      , warpToCentre >> promptedShift)
-            -- workaround
-            -- ,("M-<Tab>"    , switchNthLastFocused myTopicConfig . succ . length . W.visible . windowset =<< get )
-            -- ,("M-a"        , warpToCentre >> goToSelected gsConfig)
-            --
-            , ((modMask, xK_p), shellPromptHere myXPConfig)
-            -- Close focused window.
-            , ((modMask .|. shiftMask, xK_c), kill)
-            -- Cycle through the available layout algorithms.
-            , ((modMask, xK_space), sendMessage NextLayout)
-            --  Reset the layouts on the current workspace to default.
-            , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
-            -- Resize viewed windows to the correct size.
-            , ((modMask, xK_n), refresh)
-            -- Move focus to the next window.
-            , ((modMask, xK_Tab), windows W.focusDown)
-            -- Move focus to the next window.
-            , ((modMask, xK_j), windows W.focusDown)
-            -- Move focus to the previous window.
-            , ((modMask, xK_k), windows W.focusUp  )
-            -- Move focus to the master window.
-            , ((modMask, xK_m), windows W.focusMaster  )
-            -- Swap the focused window and the master window.
-            , ((modMask, xK_Return), windows W.swapMaster)
-            -- Swap the focused window with the next window.
-            , ((modMask .|. shiftMask, xK_j), windows W.swapDown  )
-            -- Swap the focused window with the previous window.
-            , ((modMask .|. shiftMask, xK_k), windows W.swapUp    )
-            -- Shrink the master area.
-            , ((modMask, xK_h), sendMessage Shrink)
-            -- Expand the master area.
-            , ((modMask, xK_l), sendMessage Expand)
-            -- Push window back into tiling.
-            , ((modMask, xK_t), withFocused $ windows . W.sink)
-            -- Increment the number of windows in the master area.
-            , ((modMask, xK_comma), sendMessage (IncMasterN 1))
-            -- Decrement the number of windows in the master area.
-            , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
-            -- Quit xmonad.
-            , ((modMask .|. shiftMask, xK_q), io (exitWith ExitSuccess))
-            -- Restart xmonad.
-            , ((modMask, xK_q), spawn "xmonad --recompile; xmonad --restart")
-            -- TopicSpace binds
-            , ((modMask .|. shiftMask, xK_a     ), currentTopicAction myTopicConfig)
-            , ((modMask              , xK_s     ), promptedGoto)
-            , ((modMask .|. shiftMask, xK_s     ), promptedShift)
-            ]
-            ++
-            [((modMask, k), switchNthLastFocused myTopicConfig i)
-                | (i, k) <- zip [1..] [xK_1 .. xK_9]]
-            -- ++
-            -- mod-[1..9], Switch to workspace N
-            -- mod-shift-[1..9], Move client to workspace N
-            -- [((m .|. modMask, k), windows $ f i)
-            --     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
-            --     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-            -- ++
-            -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
-            -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-            -- [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-            --     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-            --     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
